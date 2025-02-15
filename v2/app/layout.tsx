12f5405a895439aeb5c362ea16480f80a5b1bb0d
hoangdesu <hoangdesu@gmail.com>
2025-02-15 11:50:59 +0700

    disabled static build pipeline, added google analytics 4

--- v2/app/layout.tsx
+++ v2/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Brian Nguyen',
@@ -17,6 +18,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-TVLDPK85RM`}
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-TVLDPK85RM', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
+
       <body className={`antialiased ${poppins.className}`}>{children}</body>
     </html>
   );
